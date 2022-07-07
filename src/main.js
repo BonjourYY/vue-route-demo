ad19cf0360efc8f2bceab41ddac0c03af4c773e6
范勤阳 <fqymtu@gmail.com>
2022-07-07 19:56:42 +0800

    init：create vue-route-demo project ✨

--- src/main.js
+++ src/main.js
@@ -1,8 +1,36 @@
 import Vue from 'vue'
-import App from './App.vue'
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+console.log(window.location.pathname)
+
+
+const About = { template: "<p>this is about page</p>" };
+const NotFound = { template: "<p>not found</p>" };
+const Home = { template: "<p>Home page</p>" }
+
+
+const routers = {
+  "/about": About,
+  "/": Home,
+}
+
+const app = new Vue({
+  el: "#app",
+  data: () => {
+    return {
+      currentView: window.location.pathname
+    }
+  },
+  computed: {
+    view: function () {
+      return routers[this.currentView] ? routers[this.currentView] : NotFound
+    }
+  },
+  render: function (h) {
+    return h(this.view)
+  }
+})
+
+console.log(app.view)
+
