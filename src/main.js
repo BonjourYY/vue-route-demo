9cf6f535e9d48a75120378f27b69171b2a857f86
范勤阳 <fqymtu@gmail.com>
2022-07-11 18:05:59 +0800

    feature：update devtools API

--- src/main.js
+++ src/main.js
@@ -3,28 +3,8 @@ import App from "./App.vue"
 
 // Vue Global Config
 Vue.config.productionTip = false
-// Vue.config.silent = true
-
-// Vue.config.optionMergeStrategies.fqyoption = function (mixinOptions, componentOptions) {
-//   return { componentOptions, name: "343214" }
-// }
-
-// console.log(Vue.config.optionMergeStrategies)
-
-
-
-// 定义 Mixin 对象
-// let myMixin = {
-//   myoption: "q23412321",
-//   fqyoption: "fan"
-// }
-
-// 全局注册混入对象
-// Vue.mixin({
-//   created: function () {
-//     console.log(this)
-//   }
-// })
+// 是否打开控制台 devtools 选项
+Vue.config.devtools = false
 
 new Vue({
   el: "#app",
