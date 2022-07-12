27290fca3cddf87456ac188acd66e84256e72c78
范勤阳 <fqymtu@gmail.com>
2022-07-12 14:40:39 +0800

    feature：update Vue.extend() API

--- src/main.js
+++ src/main.js
@@ -1,23 +1,30 @@
 import Vue from 'vue'
-import App from "./App.vue"
+// import App from "./App.vue"
 
 // Vue Global Config
 Vue.config.productionTip = false
 
 Vue.config.performance = true;
 
-new Vue({
-  el: "#app",
-  data: () => {
-    return {
-      message: "world",
-      bar: "456"
-    }
-  },
-  render: function (h) {
-    return h(App)
-  }
+let mycomponent = Vue.extend({
+  template: "<p>我是范勤阳<p>"
 })
+
+let myComponent = new mycomponent();
+myComponent.$mount("#app")
+
+// new Vue({
+//   el: "#app",
+//   data: () => {
+//     return {
+//       message: "world",
+//       bar: "456"
+//     }
+//   },
+//   render: function (h) {
+//     return h(App)
+//   }
+// })
 
 
 
