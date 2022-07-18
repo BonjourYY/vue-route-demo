bff859ded54e54aceb1a56b40d3b89d962823782
范勤阳 <fqymtu@gmail.com>
2022-07-18 17:47:20 +0800

    feature：options/props API

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <div class="hello" :name="name">
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
@@ -8,8 +8,10 @@
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank"
+          rel="noopener">babel</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank"
+          rel="noopener">eslint</a></li>
     </ul>
     <h3>Essential Links</h3>
     <ul>
@@ -23,7 +25,8 @@
     <ul>
       <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
       <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a>
+      </li>
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
@@ -34,12 +37,14 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
-  },
-  data: () => {
-    return {
-      message: "world-3",
-      bar: "456"
+    msg: String,
+    name: {
+      type: String,
+      default: "xxx",
+      required: true,
+      validator: function (arg) {
+        return arg.toString().length > 10;
+      }
     }
   },
 }
